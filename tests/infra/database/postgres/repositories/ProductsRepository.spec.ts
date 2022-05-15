6720d9fe3cf2deda5c7c50282e66eb57fbc2e370
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-15 16:05:20 -0300

    refactor: do not repeat makeFakeProduct every test

--- tests/infra/database/postgres/repositories/ProductsRepository.spec.ts
+++ tests/infra/database/postgres/repositories/ProductsRepository.spec.ts
@@ -4,6 +4,8 @@ import { makeFakeProduct } from "@tests/domain/fakes";
 
 PostgresConnection.prototype.getRepository = jest.fn().mockReturnValue({ findOneBy: jest.fn() });
 
+const fakeProduct = makeFakeProduct();
+
 const connectionMock = new PostgresConnection();
 const productsRepositoryMock = jest.mocked(connectionMock.getRepository(Product));
 
@@ -13,21 +15,18 @@ describe("ProductsRepository", () => {
   describe("findById", () => {
     it("Should call Repository.findOneBy", async () => {
       const sut = makeSut();
-      const fakeProduct = makeFakeProduct();
       await sut.findById(fakeProduct.id);
       expect(productsRepositoryMock.findOneBy).toHaveBeenCalledTimes(1);
     });
 
     it("Should throw if Repository.findOneBy throws", async () => {
       const sut = makeSut();
-      const fakeProduct = makeFakeProduct();
       jest.spyOn(productsRepositoryMock, "findOneBy").mockRejectedValueOnce(new Error());
       await expect(sut.findById(fakeProduct.id)).rejects.toThrow();
     });
 
     it("Should return an product if id exists", async () => {
       const sut = makeSut();
-      const fakeProduct = makeFakeProduct();
 
       jest.spyOn(productsRepositoryMock, "findOneBy").mockResolvedValueOnce(fakeProduct);
       const foundProduct = await sut.findById(fakeProduct.id);
@@ -37,7 +36,6 @@ describe("ProductsRepository", () => {
 
     it("Should not return a product if id not exists", async () => {
       const sut = makeSut();
-      const fakeProduct = makeFakeProduct();
       const noneProduct = await sut.findById(fakeProduct.id);
       expect(noneProduct).toBeFalsy();
     });
