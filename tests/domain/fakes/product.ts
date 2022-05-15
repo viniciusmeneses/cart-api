e30e75cc310ccaed1805d4592c467cfd7d79a378
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-15 17:44:28 -0300

    test: find products by ID

--- tests/domain/fakes/product.ts
+++ tests/domain/fakes/product.ts
@@ -1,8 +1,8 @@
 import { Product } from "@domain/entities/Product";
 import faker from "@faker-js/faker";
 
-export const makeFakeProduct = (): Product => ({
-  id: faker.datatype.uuid(),
+export const makeFakeProduct = ({ id }: { id: string }): Product => ({
+  id,
   name: faker.random.word(),
   stock: faker.datatype.number({ min: 0 }),
   price: faker.datatype.number({ min: 0, precision: 0.01 }),
--- tests/infra/database/postgres/repositories/ProductsRepository.spec.ts
+++ tests/infra/database/postgres/repositories/ProductsRepository.spec.ts
@@ -1,10 +1,11 @@
 import { Product } from "@domain/entities/Product";
+import faker from "@faker-js/faker";
 import { PostgresConnection, ProductsRepository } from "@infra/database/postgres";
 import { makeFakeProduct } from "@tests/domain/fakes";
 
-PostgresConnection.prototype.getRepository = jest.fn().mockReturnValue({ findOneBy: jest.fn() });
+PostgresConnection.prototype.getRepository = jest.fn().mockReturnValue({ findOneBy: jest.fn(), findBy: jest.fn() });
 
-const fakeProduct = makeFakeProduct();
+const fakeProduct = makeFakeProduct({ id: faker.datatype.uuid() });
 
 const connectionMock = new PostgresConnection();
 const productsRepositoryMock = jest.mocked(connectionMock.getRepository(Product));
@@ -37,6 +38,35 @@ describe("ProductsRepository", () => {
     it("Should not return a product if id not exists", async () => {
       const sut = makeSut();
       const noneProduct = await sut.findById(fakeProduct.id);
+      expect(noneProduct).toBeFalsy();
+    });
+  });
+
+  describe("findByIds", () => {
+    it("Should call Repository.findBy", async () => {
+      const sut = makeSut();
+      await sut.findByIds([fakeProduct.id]);
+      expect(productsRepositoryMock.findBy).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should throw if Repository.findBy throws", async () => {
+      const sut = makeSut();
+      jest.spyOn(productsRepositoryMock, "findBy").mockRejectedValueOnce(new Error());
+      await expect(sut.findByIds([fakeProduct.id])).rejects.toThrow();
+    });
+
+    it("Should return products if ids exists", async () => {
+      const sut = makeSut();
+
+      jest.spyOn(productsRepositoryMock, "findBy").mockResolvedValueOnce([fakeProduct]);
+      const foundProduct = await sut.findByIds([fakeProduct.id]);
+
+      expect(foundProduct).toEqual([fakeProduct]);
+    });
+
+    it("Should not return products if ids not exists", async () => {
+      const sut = makeSut();
+      const noneProduct = await sut.findByIds([fakeProduct.id]);
       expect(noneProduct).toBeFalsy();
     });
   });
