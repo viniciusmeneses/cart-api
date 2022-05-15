c48d8376b014c70e184c813fe3fb4c3d44a6bc0f
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-15 20:53:43 -0300

    feat: add create cart controller

---
+++ src/presentation/controllers/cart/CreateCartController.ts
@@ -0,0 +1,43 @@
+import { inject, singleton } from "tsyringe";
+
+import { ICreateCartUseCase } from "@domain/ports/useCases/cart/ICreateCartUseCase";
+import { ProductNoStockAvailable, ProductNotExistsError } from "@domain/useCases/errors";
+import { ValidationErrors } from "@domain/validator";
+import { HttpResponse } from "@presentation/helpers";
+import { Http, IController } from "@presentation/protocols";
+
+@singleton()
+export class CreateCartController implements IController {
+  public constructor(@inject("CreateCartUseCase") private createCartUseCase: ICreateCartUseCase) {}
+
+  public async handle(request: CreateCartController.IRequest): Promise<Http.IResponse> {
+    const { items } = request.body;
+
+    try {
+      const cart = await this.createCartUseCase.execute({ items });
+      return HttpResponse.created(cart);
+    } catch (error) {
+      return this.handleError(error);
+    }
+  }
+
+  private handleError(error: Error): Http.IResponse {
+    if (error instanceof ValidationErrors) return HttpResponse.badRequest(error.errors);
+    if (error instanceof ProductNotExistsError) return HttpResponse.badRequest(error);
+    if (error instanceof ProductNoStockAvailable) return HttpResponse.badRequest(error);
+    throw error;
+  }
+}
+
+export namespace CreateCartController {
+  interface IRequestItem {
+    productId: string;
+    amount: number;
+  }
+
+  interface IRequestBody {
+    items: IRequestItem[];
+  }
+
+  export type IRequest = Http.IRequest<IRequestBody>;
+}
