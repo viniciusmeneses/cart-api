91681bfc511502d33255577db4b75d121ddb74d2
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-16 16:47:02 -0300

    test: add 201 http response helper method

--- tests/presentation/helpers/HttpResponse.spec.ts
+++ tests/presentation/helpers/HttpResponse.spec.ts
@@ -4,6 +4,20 @@ import { HttpResponse } from "@presentation/helpers";
 const makeSut = () => HttpResponse;
 
 describe("HttpResponse", () => {
+  describe("ok", () => {
+    it("Should return status code 200", async () => {
+      const sut = makeSut();
+      const response = sut.ok(null);
+      expect(response.status).toBe(200);
+    });
+
+    it("Should return data as body", async () => {
+      const sut = makeSut();
+      const response = sut.ok({ any: "data" });
+      expect(response.body).toEqual({ any: "data" });
+    });
+  });
+
   describe("created", () => {
     it("Should return status code 201", async () => {
       const sut = makeSut();
@@ -34,6 +48,26 @@ describe("HttpResponse", () => {
       });
 
       const errorResponse = sut.badRequest(new Error("error"));
+      expect(errorResponse.body).toEqual({ errors: [{ type: "Error", message: "error" }] });
+    });
+  });
+
+  describe("notFound", () => {
+    it("Should return status code 404", async () => {
+      const sut = makeSut();
+      const response = sut.notFound(new Error("error"));
+      expect(response.status).toBe(404);
+    });
+
+    it("Should return array of errors as body", async () => {
+      const sut = makeSut();
+
+      const validationResponse = sut.notFound([new FieldValidationError("field", "error")]);
+      expect(validationResponse.body).toEqual({
+        errors: [{ type: "FieldValidationError", field: "field", message: "error" }],
+      });
+
+      const errorResponse = sut.notFound(new Error("error"));
       expect(errorResponse.body).toEqual({ errors: [{ type: "Error", message: "error" }] });
     });
   });
