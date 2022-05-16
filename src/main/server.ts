2ef9889bf83dfa168e107eaad49e9da4cd842b8d
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-15 21:01:57 -0300

    feat: start http server after connect to database

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,2 +1,16 @@
 import "reflect-metadata";
 import "dotenv/config";
+import "./container";
+
+import { container } from "tsyringe";
+
+import { PostgresConnection } from "@infra/database/postgres";
+
+import { app } from "./app";
+
+const PORT = process.env.PORT;
+
+container
+  .resolve(PostgresConnection)
+  .connect()
+  .then(() => app.listen(PORT, () => console.log(`API listening at port ${PORT}`)));
