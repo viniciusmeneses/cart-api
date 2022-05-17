156546e8a9d7c5f0664eb255ad25286b1737c6cc
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-16 21:20:07 -0300

    fix: register cart item repository and use case in tsyring container

--- src/main/container.ts
+++ src/main/container.ts
@@ -1,12 +1,14 @@
 import { container } from "tsyringe";
 
-import { ICartsRepository, IProductsRepository } from "@domain/ports/repositories";
-import { ICreateCartUseCase, ILoadCartUseCase } from "@domain/ports/useCases/cart";
-import { CreateCartUseCase, LoadCartUseCase } from "@domain/useCases/cart";
-import { CartsRepository, ProductsRepository } from "@infra/database/postgres";
+import { ICartItemsRepository, ICartsRepository, IProductsRepository } from "@domain/ports/repositories";
+import { ICreateCartUseCase, ILoadCartUseCase, IRemoveCartItemsUseCase } from "@domain/ports/useCases/cart";
+import { CreateCartUseCase, LoadCartUseCase, RemoveCartItemsUseCase } from "@domain/useCases/cart";
+import { CartItemsRepository, CartsRepository, ProductsRepository } from "@infra/database/postgres";
 
 container.registerSingleton<IProductsRepository>("ProductsRepository", ProductsRepository);
 container.registerSingleton<ICartsRepository>("CartsRepository", CartsRepository);
+container.registerSingleton<ICartItemsRepository>("CartItemsRepository", CartItemsRepository);
 
 container.registerSingleton<ICreateCartUseCase>("CreateCartUseCase", CreateCartUseCase);
 container.registerSingleton<ILoadCartUseCase>("LoadCartUseCase", LoadCartUseCase);
+container.registerSingleton<IRemoveCartItemsUseCase>("RemoveCartItemsUseCase", RemoveCartItemsUseCase);
