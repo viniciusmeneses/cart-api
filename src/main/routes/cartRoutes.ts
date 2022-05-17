9e0d5a6a0d2c6f5acc744eaa66b795908ff18ddf
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-16 22:00:15 -0300

    feat: add controller and route to remote items from cart

--- src/main/routes/cartRoutes.ts
+++ src/main/routes/cartRoutes.ts
@@ -4,10 +4,12 @@ import { container } from "tsyringe";
 import { adaptExpressRoute } from "@main/helpers";
 import { CreateCartController } from "@presentation/controllers/cart";
 import { LoadCartController } from "@presentation/controllers/cart/LoadCartController";
+import { RemoveCartItemsController } from "@presentation/controllers/cart/RemoveCartItemsController";
 
 const cartRoutes = Router();
 
 cartRoutes.post("/", adaptExpressRoute(container.resolve(CreateCartController)));
 cartRoutes.get("/:id", adaptExpressRoute(container.resolve(LoadCartController)));
+cartRoutes.delete("/:id/items", adaptExpressRoute(container.resolve(RemoveCartItemsController)));
 
 export { cartRoutes };
---
+++ src/presentation/controllers/cart/RemoveCartItemsController.ts
@@ -0,0 +1,33 @@
+import { inject, singleton } from "tsyringe";
+
+import { IRemoveCartItemsUseCase } from "@domain/ports/useCases/cart";
+import { CartNotExistsError } from "@domain/useCases/errors";
+import { ValidationErrors } from "@domain/validator";
+import { HttpResponse } from "@presentation/helpers";
+import { Http, IController } from "@presentation/protocols";
+
+@singleton()
+export class RemoveCartItemsController implements IController {
+  public constructor(@inject("RemoveCartItemsUseCase") private removeCartItemsUseCase: IRemoveCartItemsUseCase) {}
+
+  public async handle(request: RemoveCartItemsController.IRequest): Promise<Http.IResponse> {
+    const { id: cartId } = request.url.params;
+
+    try {
+      await this.removeCartItemsUseCase.execute({ cartId });
+      return HttpResponse.noContent();
+    } catch (error) {
+      return this.handleError(error);
+    }
+  }
+
+  private handleError(error: Error): Http.IResponse {
+    if (error instanceof ValidationErrors) return HttpResponse.badRequest(error.errors);
+    if (error instanceof CartNotExistsError) return HttpResponse.notFound(error);
+    throw error;
+  }
+}
+
+export namespace RemoveCartItemsController {
+  export type IRequest = Http.IRequest<unknown, { id: string }>;
+}
