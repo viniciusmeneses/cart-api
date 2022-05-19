9ab3c5d57b62b38e1d8d8ec5d12d187c78a69cdc
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-19 09:04:48 -0300

    feat: serve swagger ui

--- src/main/app.ts
+++ src/main/app.ts
@@ -3,6 +3,9 @@ import "express-async-errors";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
+import swaggerUI from "swagger-ui-express";
+
+import swaggerDocument from "../../swagger.json";
 
 import { errorHandlerMiddleware } from "./middlewares";
 import { routes } from "./routes";
@@ -12,7 +15,10 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(routes);
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use("/api", routes);
+
 app.use(errorHandlerMiddleware);
 
 export { app };
