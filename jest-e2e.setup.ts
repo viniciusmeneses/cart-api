ad7c0d4435bac0c3fe9ed420ed0eaaef26872acc
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-05-20 09:22:28 -0300

    chore: setup e2e tests

--- jest-e2e.setup.ts
+++ jest-e2e.setup.ts
@@ -1,1 +1,17 @@
 import "./src/main/container";
+
+import { createDatabase, dropDatabase } from "typeorm-extension";
+
+import dataSource from "./src/infra/database/postgres/dataSource";
+
+beforeAll(() =>
+  createDatabase({
+    options: dataSource.options,
+  })
+);
+
+afterAll(() =>
+  dropDatabase({
+    options: dataSource.options,
+  })
+);
